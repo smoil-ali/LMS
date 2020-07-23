5650b04d951e48ba63e06f61ffaa8f83053dc92b
UsamaSarfraz1 <49511695+UsamaSarfraz1@users.noreply.github.com>
2020-07-23 19:36:08 +0500

    by usama

--- app/src/main/java/com/example/lms/ui/students/StudentFragment.java
+++ app/src/main/java/com/example/lms/ui/students/StudentFragment.java
@@ -1,9 +1,11 @@
 package com.example.lms.ui.students;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,7 +19,15 @@ public class StudentFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root= inflater.inflate(R.layout.fragment_students,container,false);
-
+        root.findViewById(R.id.student_card).setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu popupMenu=new PopupMenu(requireContext(),root.findViewById(R.id.student_card));
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu,popupMenu.getMenu());
+                popupMenu.show();
+                return true;
+            }
+        });
         return root;
     }
 }
